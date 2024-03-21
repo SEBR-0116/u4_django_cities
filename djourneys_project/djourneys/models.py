8418cadab86e1e4c370e6b06f4b1af81a78cdbaa
kevin <xinjiewang2882@gmail.com>
2024-03-21 14:31:54 -0400

    1st

---
+++ djourneys_project/djourneys/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    discription = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class Attraction(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='attractions')
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    text = models.TextField()
+    rating = models.IntegerField()
+    attraction = models.ForeignKey(Attraction, on_delete=models.CASCADE, related_name='reviews')
+    
+    def __str__(self):
+        return f"Review for {self.attraction.name}"
---
+++ djourneys_project/settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE djourneys;
+CREATE USER djourneysuser WITH PASSWORD 'djourneys';
+GRANT ALL PRIVILEGES ON DATABASE djourneys TO djourneysuser;
