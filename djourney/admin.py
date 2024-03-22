f3c9042e1845e3614e73281fb6ccad8238857a73
wilmckay <trifarce789@gmail.com>
2024-03-22 00:02:41 -0400

    Wil McKay

---
+++ djourney/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Cities, Attractions, Reviews
+admin.site.register(Cities)
+admin.site.register(Attractions)
+admin.site.register(Reviews)
+# Register your models here.
