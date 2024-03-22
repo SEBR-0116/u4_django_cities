31c497887f8755dcb88fd726767229f11cc7bf97
Felix Man <felixwhman@gmail.com>
2024-03-22 16:06:18 -0400

    Part 1 completed

---
+++ djourneys_project/djourneys/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    population = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Attraction(models.Model):
+    city = models.ForeignKey(
+        City, on_delete=models.CASCADE, related_name='attractions')
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    attraction = models.ForeignKey(
+        Attraction, on_delete=models.CASCADE, related_name='reviews')
+    title = models.CharField(max_length=100)
+    rating = models.IntegerField()
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
