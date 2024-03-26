26904d5529da626d88e1e101237571b481c174b6
Felix Man <felixwhman@gmail.com>
2024-03-25 22:06:13 -0400

    Fixed reviews and attractions endpoint

--- djourneys_project/djourneys/serializers.py
+++ djourneys_project/djourneys/serializers.py
@@ -2,32 +2,50 @@ from rest_framework import serializers
 from .models import City, Attraction, Review
 
 
-class CitySerializer(serializers.HyperlinkedModelSerializer):
+class ReviewSerializer(serializers.HyperlinkedModelSerializer):
     attractions = serializers.HyperlinkedRelatedField(
         view_name='attraction_detail',
-        many=True,
         read_only=True
     )
 
+    review = serializers.HyperlinkedIdentityField(
+        view_name='review_detail'
+    )
+
     class Meta:
-        model = City
-        fields = ('name', 'country', 'population', 'attractions')
+        model = Review
+        fields = ('id', 'attractions', 'review', 'title', 'rating', 'content')
 
 
 class AttractionSerializer(serializers.HyperlinkedModelSerializer):
+    city = serializers.HyperlinkedRelatedField(
+        view_name='review_detail',
+        read_only=True,
+    )
+
     reviews = serializers.HyperlinkedRelatedField(
         view_name='review_detail',
         many=True,
         read_only=True,
     )
+    attraction_url = serializers.HyperlinkedIdentityField(
+        view_name='attraction_detail')
 
     class Meta:
         model = Attraction
-        fields = ('city', 'name', 'description', 'type', 'reviews')
+        fields = ('id', 'attraction_url', 'city', 'name',
+                  'description', 'type', 'reviews')
 
 
-class ReviewSerializer(serializers.HyperlinkedModelSerializer):
+class CitySerializer(serializers.HyperlinkedModelSerializer):
+    attractions = serializers.HyperlinkedRelatedField(
+        view_name='attraction_detail',
+        many=True,
+        read_only=True,
+    )
+    city_url = serializers.HyperlinkedIdentityField(view_name='city_detail')
 
     class Meta:
-        model = Review
-        fields = ('attraction', 'title', 'rating', 'content')
+        model = City
+        fields = ('id', 'city_url', 'name', 'country',
+                  'population', 'attractions')
--- djourneys_project/djourneys/urls.py
+++ djourneys_project/djourneys/urls.py
@@ -4,11 +4,12 @@ from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('cities/', views.CityList.as_view(), name='city_list'),
-    path('cities/<int:pk>/', views.CityDetail.as_view(), name='city_detail'),
-    path('attractions/', views.AttractionList.as_view(), name="attraction_list"),
+    path('citys/<int:pk>/', views.CityDetail.as_view(), name='city_detail'),
+
+    path('attractions/', views.AttractionList.as_view(), name='attraction_list'),
     path('attractions/<int:pk>/', views.AttractionDetail.as_view(),
          name='attraction_detail'),
+
     path('reviews/', views.ReviewList.as_view(), name='review_list'),
-    path('reviews/<int:pk>/', views.ReviewDetail.as_view(),
-         name='review_detail')
+    path('reviews/<int:pk>/', views.ReviewDetail.as_view(), name='review_detail')
 ]
